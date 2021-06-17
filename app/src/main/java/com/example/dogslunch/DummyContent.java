0f55dc2635c123ad42b05cd0878d134e041b854b
Ivan <ivan.belov14.01@gmail.com>
2021-06-17 22:04:06 +0700

    configuring the list and detailed information

---
+++ app/src/main/java/com/example/dogslunch/DummyContent.java
@@ -0,0 +1,34 @@
+package com.example.dogslunch;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DummyContent {
+    /** Списочный массив элементов */
+    public static final List<DummyItem> ITEMS = new ArrayList<DummyItem>();
+    private static final int COUNT = 50;
+
+    static {
+        // Добавление элементов в список.
+        for (int k = 1; k <= COUNT; k++) {
+            StringBuilder builder = new StringBuilder();
+            for (int j = 0; j < k; j++) { builder.append("\n Детальная информация. "); }
+            ITEMS.add(new DummyItem(String.valueOf(k), "Элемент " + k, builder.toString()));
+        }
+    }
+
+    /** Создание класса элементов для списка. */
+    public static class DummyItem {
+        public final String id;
+        public final String content;
+        public final String details;
+
+        public DummyItem(String id, String content, String details) {
+            this.id = id;
+            this.content = content;
+            this.details = details;
+        }
+        @Override
+        public String toString() { return content; }
+    }
+}
